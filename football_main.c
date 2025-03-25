97d2aca8fe3976679379533a05bb2c775cf909f4
twebb810 <120744535+twebb810@users.noreply.github.com>
2025-03-25 10:46:24 -0500

    Update football_main.c

--- football_main.c
+++ football_main.c
@@ -1,5 +1,25 @@
+#include <stdio.h>
 #include "football.h"
 
 int main(int argc, char *argv[]) {
+    int score;
+
+    while (1) {
+        printf("\nEnter the NFL score (Enter 0 or 1 to STOP): ");
+        if (scanf("%d", &score) != 1) {
+            printf("Invalid input. Please enter a number.\n");
+            while (getchar() != '\n');  // Clear input buffer
+            continue;
+        }
+
+        if (score <= 1) {
+            printf("Program terminated.\n");
+            break;
+        }
+
+        printf("Total combinations: %d\n", count_combinations(score));
+        print_combinations(score);
+    }
+
     return 0;
 }
